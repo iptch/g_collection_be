d46b71a0cc59cc557b4a23e77547a6ffef3cd630
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 19:01:54 +0200

    adjusting production.py

--- genius_collection/production.py
+++ genius_collection/production.py
@@ -26,7 +26,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # STORAGES = {
--- genius_collection/settings.py
+++ genius_collection/settings.py
@@ -130,3 +130,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
