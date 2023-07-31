a3774a8cd3df639674ededb671da4de019f48273
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 10:36:25 +0200

    initial

---
+++ genius_collection/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for genius_collection project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genius_collection.settings')
+
+application = get_asgi_application()
---
+++ genius_collection/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for genius_collection project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genius_collection.settings')
+
+application = get_wsgi_application()
