573148d78f0cc6f05ee557bf86de2c64d34f45ac
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 17:35:49 +0200

    adjusting production.py

--- genius_collection/production.py
+++ genius_collection/production.py
@@ -10,7 +10,7 @@ from .settings import BASE_DIR
 # # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-# DEBUG = False
+DEBUG = False
 #
 # # WhiteNoise configuration
 # MIDDLEWARE = [
