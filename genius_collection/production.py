0e40495b8e8480e2b0c5e8d18639d4b1962f6c5e
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 19:54:22 +0200

    use postgres instead sqlite

--- genius_collection/production.py
+++ genius_collection/production.py
@@ -4,7 +4,6 @@
 import os
 
 from .settings import *  # noqa
-from .settings import BASE_DIR
 #
 # # Configure the domain name using the environment variable
 # # that Azure automatically creates for us.
@@ -26,25 +25,17 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-# STORAGES = {
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
-# }
-#
-# # Configure Postgres database based on connection string of the libpq Keyword/Value form
-# # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-# conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': conn_str_params['dbname'],
-#         'HOST': conn_str_params['host'],
-#         'USER': conn_str_params['user'],
-#         'PASSWORD': conn_str_params['password'],
-#     }
-# }
+# Configure Postgres database based on connection string of the libpq Keyword/Value form
+# https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password'],
+    }
+}
