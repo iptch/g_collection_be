0b99c5662ebe0ba4e416ad3cd82e6f735ea824fc
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 19:08:57 +0200

    adjusting production.py

--- genius_collection/settings.py
+++ genius_collection/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
