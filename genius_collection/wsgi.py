1e4e36dfb004b43a1074b5cee09e7d5f0192ea32
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 22:15:32 +0200

    change postgres settings

--- genius_collection/wsgi.py
+++ genius_collection/wsgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-settings_module = 'genius_collection.production' if 'WEBSITE_HOSTNAME' in os.environ else 'genius_collection.settings'
+is_prod = 'WEBSITE_HOSTNAME' in os.environ
+settings_module = 'genius_collection.production' if is_prod else 'genius_collection.settings'
+# settings_module = 'genius_collection.production' if 'WEBSITE_HOSTNAME' in os.environ else 'genius_collection.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,10 +3,19 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genius_collection.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genius_collection.settings')
+    is_prod = 'WEBSITE_HOSTNAME' in os.environ
+    if not is_prod:
+        print("Loading environment variables from .env file")
+        load_dotenv('./.env')
+
+    settings_module = "genius_collection.production" if is_prod else 'genius_collection.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
