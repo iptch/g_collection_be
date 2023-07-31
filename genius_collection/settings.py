d73659fa40bd7ca588e3a89cd0388dd41a7eba95
Christoph Weber <christoph.t.weber@gmail.com>
2023-07-31 14:41:36 +0200

    moved secret_key to .env

--- genius_collection/settings.py
+++ genius_collection/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret! # TODO
-SECRET_KEY = 'django-insecure-3_50ia13a=fx$hqvp$_5tw=nccb!ca+m@gr@xfbe2b6qd1mc7&'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,17 +83,6 @@ DATABASES = {
     }
 }
 
-# # Configure Postgres database for local development
-# #  Set these environment variables in the .env file for this project.
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DBNAME'),
-#         'HOST': os.environ.get('DBHOST'),
-#         'USER': os.environ.get('DBUSER'),
-#         'PASSWORD': os.environ.get('DBPASS'),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
