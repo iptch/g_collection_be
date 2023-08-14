6f671bc0bc6ea7c7955242d0b721e6a7d6ddef82
Christoph Weber <christoph.t.weber@gmail.com>
2023-08-14 10:01:57 +0200

    cors

--- genius_collection/settings.py
+++ genius_collection/settings.py
@@ -29,12 +29,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +121,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CORS_ALLOWED_ORIGINS = [
+    "https://g-collection.azurewebsites.net",
+    "http://localhost:4200",
+]
