9b03d1c490ea5e4c03c1fe719fd6ab679721832b
Vinu <kevinduss1@gmail.com>
2023-09-08 09:45:23 +0000

    add devcontainer config

--- genius_collection/core/serializers.py
+++ genius_collection/core/serializers.py
@@ -12,4 +12,4 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class CardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Card
-        fields = ['name', 'acronym', 'team', 'job', 'superpower', 'highlight', 'must_have', 'image_link']
+        fields = ['id', 'name', 'acronym', 'team', 'job', 'superpower', 'highlight', 'must_have', 'image_link']
