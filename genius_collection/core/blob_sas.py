06a91ddd1288693d1f17915210e0ab8192a3575d
Vinu <kevinduss1@gmail.com>
2023-09-10 17:00:05 +0000

    switch azure blob container

--- genius_collection/core/blob_sas.py
+++ genius_collection/core/blob_sas.py
@@ -20,7 +20,7 @@ def request_user_delegation_key(blob_service_client: BlobServiceClient) -> UserD
         key_expiry_time=delegation_key_expiry_time
     )
 
-def create_user_delegation_blob_sas(blob_client: BlobClient, user_delegation_key: UserDelegationKey):
+def create_user_delegation_blob_sas(blob_client: BlobClient, user_delegation_key: UserDelegationKey) -> str:
         # Create a SAS token that's valid for 1 hour
         start_time = datetime.datetime.now(datetime.timezone.utc)
         expiry_time = start_time + datetime.timedelta(hours=1)
@@ -39,6 +39,6 @@ def get_blob_sas_url(blob_name: str) -> str:
     account_url = "https://gcollection.blob.core.windows.net"
     blob_service_client = BlobServiceClient(account_url, credential=DefaultAzureCredential())
     user_delegation_key = request_user_delegation_key(blob_service_client=blob_service_client)
-    blob_client = blob_service_client.get_blob_client(container="portraits", blob=blob_name)
+    blob_client = blob_service_client.get_blob_client(container="card-high-res-images", blob=blob_name)
     sas_token = create_user_delegation_blob_sas(blob_client=blob_client, user_delegation_key=user_delegation_key)
     return f"{blob_client.url}?{sas_token}"
