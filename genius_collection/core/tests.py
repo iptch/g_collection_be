d1ceb3311745117386bf4c46982bfeb0b220d2bc
Manuel Kuchelmeister <manuel.kuchelmeister@web.de>
2023-09-29 16:16:05 +0200

    Draft: Test assign_ownership method

--- genius_collection/core/tests.py
+++ genius_collection/core/tests.py
@@ -1,3 +1,17 @@
+from genius_collection.core.models import User, Card, Ownership
 from django.test import TestCase
+from django.db.models import Sum
 
 # Create your tests here.
+def test_assign_ownership():
+    num_samples = 5
+    # TODO: This currently only works with a populated local database and 
+    # without using TestCase -> Rewrite as integration test
+    user = User.objects.all()[0]
+    manager = Ownership.objects
+    ownerships_before = Ownership.objects.aggregate(Sum("quantity"))['quantity__sum']
+    manager.assign_ownership(user, num_samples=num_samples)
+    ownerships_after = Ownership.objects.aggregate(Sum("quantity"))['quantity__sum']
+    assert num_samples == (ownerships_after - ownerships_before)
+
+test_assign_ownership()
