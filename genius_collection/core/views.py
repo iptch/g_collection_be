ef1fde3989707528d04d1e9cd1f8dd741b4eb427
ovanov <grepyugo@protonmail.com>
2023-09-29 16:21:44 +0200

    added otp check

--- genius_collection/core/views.py
+++ genius_collection/core/views.py
@@ -37,7 +37,9 @@ class CardViewSet(viewsets.ModelViewSet):
             ownership: Ownership = Ownership.objects.get(user=giver, card=card)
         except Ownership.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST, data=f"Oh Brate, Ownership does not exist!, {request.headers.items}")
-        
+
+        if ownership.otp != request.data["otp"]:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"status": f"Your otp does not match the one in the Database innit."})
         
         OwnershipHelper.transfer_ownership(request.user, ownership, card)
         
