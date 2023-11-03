9bbf0ebbab3986c9dbdfcf62f76a729bd7765c44
Vinu <kevinduss1@gmail.com>
2023-11-03 19:25:48 +0000

    refactor sas retrieval for better performance

--- genius_collection/core/blob_sas.py
+++ genius_collection/core/blob_sas.py
@@ -9,19 +9,23 @@ from azure.storage.blob import (
 )
 
 STORAGE_ACCOUNT = "gcollection"
+HOST = f"https://{STORAGE_ACCOUNT}.blob.core.windows.net"
 
 
 # Microsoft recommends the use of Azure AD credentials as a security best practice,
 # rather than using the account key, which can be more easily compromised.
-def request_user_delegation_key(blob_service_client: BlobServiceClient) -> UserDelegationKey:
+def request_user_delegation_key() -> UserDelegationKey:
     cache_key = "user_delegation_key"
     cached_key = cache.get(cache_key)
     if cached_key:
         return cached_key
 
     # If the key isn't in cache, fetch a new one.
+    print("Requesting user delegation key")
     start_time = datetime.datetime.now(datetime.timezone.utc)
     expiry_time = start_time + datetime.timedelta(days=1)
+
+    blob_service_client = BlobServiceClient(HOST, credential=DefaultAzureCredential())
 
     user_delegation_key = blob_service_client.get_user_delegation_key(
         key_start_time=start_time,
@@ -36,7 +40,6 @@ def request_user_delegation_key(blob_service_client: BlobServiceClient) -> UserD
 
 
 def create_container_sas(
-        blob_service_client: BlobServiceClient,
         user_delegation_key: UserDelegationKey,
         container_name: str
 ) -> str:
@@ -45,11 +48,12 @@ def create_container_sas(
         return cached_sas
 
     # If the SAS token isn't in cache, create a new one.
+    print("Creating container SAS token")
     start_time = datetime.datetime.now(datetime.timezone.utc)
     expiry_time = start_time + datetime.timedelta(days=1)
 
     container_sas = generate_container_sas(
-        account_name=blob_service_client.account_name,
+        account_name=STORAGE_ACCOUNT,
         container_name=container_name,
         user_delegation_key=user_delegation_key,
         permission=BlobSasPermissions(read=True),
@@ -66,9 +70,6 @@ def create_container_sas(
 
 def get_blob_sas_url(container_name: str, acronym: str) -> str:
     blob_name = f'{acronym.lower()}.jpg'
-    account_url = f"https://{STORAGE_ACCOUNT}.blob.core.windows.net"
-    blob_service_client = BlobServiceClient(account_url, credential=DefaultAzureCredential())
-    user_delegation_key = request_user_delegation_key(blob_service_client=blob_service_client)
-    sas_token = create_container_sas(blob_service_client, user_delegation_key, container_name)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    return f"{blob_client.url}?{sas_token}"
+    user_delegation_key = request_user_delegation_key()
+    sas_token = create_container_sas(user_delegation_key, container_name)
+    return f"{HOST}/{container_name}/{blob_name}?{sas_token}"
